394f615fe4affb9d35b693400ac71d9efb43c67b
lbelford23 <lochlan.belford@gmail.com>
2023-09-19 15:40:30 -0400

    Attempts to add coding question to midterm template

---
+++ MidtermCodingQuestion1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool right = Input.GetKey(KeyCode.RightArrow);
+        bool left = Input.GetKey(KeyCode.LeftArrow);
+
+        if (right)
+        {
+            transform.position = transform.position + new Vector3(1, 0, 0) * Time.deltaTime;
+        }
+
+        if (left)
+        {
+            transform.position = transform.position + new Vector3(-1, 0, 0) * Time.deltaTime;
+        }
+
+        //Finish this code so that the character can move up and down, as well as left and right
+    }
+}
